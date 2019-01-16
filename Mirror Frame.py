41301486cbd8d13453b38d18d0bb09a3332081ac
leyla-el <45810816+leyla-el@users.noreply.github.com>
2019-01-16 20:10:58 +0800

    Update Mirror Frame.py

--- Mirror Frame.py
+++ Mirror Frame.py
@@ -3,27 +3,25 @@ tom = Turtle()
 tom.shape('turtle')
 tom.speed(0)
 tom.color('#12c99e')
-
+tom.tracer(100)
 def flower():
-	for j in range (1):
-		w=1.2
+	w=1.2
+	tom.width(w)
+	for i in range(38):
+		tom.fd(3.6)
+		tom.lt(i/0.5)
+		w=w+0.05
 		tom.width(w)
-		for i in range(38):
-			tom.fd(3.6)
-			tom.lt(i/0.5)
-			w=w+0.05
-			tom.width(w)
-	for j in range (1):
-		w=0.5
+	w=0.5
+	tom.width(w)
+	for i in range(38):
+		tom.fd(3)
+		tom.rt(i/10)
+		w=w+0.1
 		tom.width(w)
-		for i in range(38):
-			tom.fd(3)
-			tom.rt(i/10)
-			w=w+0.1
-			tom.width(w)
-
 
 for count in range (8):
 	flower()
         
 tom.hideturtle()
+tom.update()
